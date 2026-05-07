8ff079db4425f824802e3ba04e94aad8b86569f2
atharvb02-hub <atharvb02@gmail.com>
2026-05-07 22:07:46 +0530

    Delete LAB Assignment -3/myapp directory

--- LAB Assignment -3/myapp/src/data.js
+++
@@ -1,8 +0,0 @@
-let students = [
-  { name: 'Adhi', marks: 80 },
-  { name: 'Sarvagya', marks: 70 },
-  { name: 'Abhi', marks: 30 },
-  { name: 'Ajitesh', marks: 20 }
-];
-
-export default students;
